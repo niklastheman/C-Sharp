dbe652c0e7d0067e97e56c43ef2731f59db056e6
niklastheman <e.niklas.holmstrom@gmail.com>
2020-08-05 11:10:17 +0200

    Övninguppgift 1

---
+++ Uppgift1_Felande/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Uppgift1_Felande
+{
+    class Program
+    {
+        /// <summary>
+        /// Korrigera koden så att programmet kan köras ordentligt. Det finns många fel i koden. 
+        /// Tips för att lösa uppgiften. Hovra över det som är rött i koden och se om felmeddelandena kan hjälpa dig.
+        /// Om du kör fast helt kan du även kolla i projektet Uppgift1_Korrekt för att se hur ett fungerande programmer ser ut.
+        /// Kör hårt!
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        
+            Console.WriteLine("Hej, välkommen till uppgift 1!");
+            Console.WriteLine("Tryck på en knapp för att fortsätta...");
+            Console.ReadKey();
+
+            namn = "Linda";
+
+            Console.WriteLine;
+            Console.WriteLine($"Här ser du en sträng representerat av variablen 'namn': {namn}");
+            Console.WriteLine("Tryck på en knapp för att fortsätta...");
+            Console.ReadKey();
+
+            Console.WriteLine();
+            Console.WriteLine($"Boolean eller bool är en användbar datatyp när man vill avgör om något är sant eller falskt. Här ser vi variablen sants värde: {sant}");
+            Console.WriteLine("Tryck på en knapp för att fortsätta...");
+            Console.ReadKey();
+
+            bool sant = false;
+
+            DateTime datum = DateaTime.Now;
+
+            Console.WriteLine();
+            Console.ReadKey($"Här ser du ett datum representerat av variablen 'datum': {datum}")
+            ConsoleWriteLine("Tryck på en knapp för att fortsätta...");
+            Console.ReadKey();
+
+            decimal decimaltal = 0.5;
+
+            Console.WriteLine();
+            Console.WriteLine($"Här ser du ett flyttal representerat av variablen 'decimaltal': {decimaltal});
+            Console.WriteLine("Tryck på en knapp för att fortsätta...");
+
+            string helttal = 30;
+
+            Console.WriteLine();
+            Console.WriteLine($"Här ser du ett heltal representerat av variablen 'heltal': {helttal}");
+            Console.WriteLine("Tryck på en knapp för att fortsätta...");
+            Console.ReadKey();
+
+            Console.ReadKey();
+        }
+    }
+}
