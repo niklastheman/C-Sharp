588e6fea8262a8b0df02cda82bba457102e39d4d
dv1993 <vahid.dennis@gmail.com>
2020-09-02 14:21:27 +0200

    uppgift 1b

---
+++ Uppgift1b/Uppgift1b/Uppgift1b/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace Uppgift1b
+{
+    class Program
+    {
+
+        //Lös uppgifterna som står i kommentarerna.
+        //All kod ska ligga inne i Main-funktionen nedan.
+        //Tips: Lägg en Console.Readline längst ner i Main-funktionen så att inte konsolen 
+        //stängs direkt.
+        static void Main(string[] args)
+        {
+            //Variabel deklarationer.
+            //Inga variabler förutom dessa ska användas.
+            //Man får inte ändra på typerna av dessa variabler.
+            int i, tal1, tal2;
+            long l;
+            decimal d;
+            float f;
+            bool b;
+            string sant, input1, input2;
+
+
+            //1. tilldela variabel i värdet 10
+            //Kod här
+            
+            //2. Använd implicit konvertering för att tilldela variabel "l" värdet av "i"
+            //Kod här
+            
+            //3. Addera 1 till variabeln "l" mha inkrement operatorn.
+            //Kod här
+            
+            //4. Använd compound assignment för att multiplicera värdet av variabel "l" med 2
+            //Kod här
+            
+            //5. Avänd explicit konvertering för att tilldela variabel "i" värdet av "l"
+            //Kod här
+            
+
+            //6. Skriv ut värdet av variabeln i konsolen. Talet 22 bör skrivas ut
+            //Kod här
+            
+
+            //7. Tilldela variabeln "f" värdet 3.14
+            //Kod här
+            
+
+            //8. Använd explicit konvertering för att tilldela variabeln "d" värdet av variabeln "f"
+            //Kod här
+            
+
+            //9. Använd dekrement operatorn för att subtrahera värdet av "d" med 1
+            //Kod här
+            
+
+            //10. Använd compound assignment för att multiplicera värdet av variabel "d" med 3
+            //Kod här
+            
+
+            //11. Använd explicit konvertering för att tilldela "i" värdet av "d" modulo 5 (restvärdet)
+            //Kod här
+            
+
+
+            //12. Skriv ut värdet av "i" i konsolen. Bör vara 1.
+            //Kod här
+            
+
+
+            //13. Tilldela variabeln "sant" värdet true som en sträng
+            //Kod här
+            
+
+            //14. Använd en hjälp klass för att konvertera 
+            //och tilldela variabeln "b" värdet av variabeln "sant"
+            //Kod här
+            
+
+            //15. Sätt variabel "b":s värde till falskt
+            //Kod här
+            
+
+            //16. Använd en hjälp klass för att konvertera 
+            //och tilldela variabeln "sant" värdet av variabeln "b"
+            //Kod här
+            
+
+            //17. Skriv ut värdet av variabeln "sant" i konsolen. Bör vara False.
+            //Kod här
+            
+
+            //18. Skriv ut till konsolen texten: Ange ett heltal:
+            //Kod här
+            
+
+            //19. Läs in ett heltal från konsolen tilldela värdet till variabel "input1"
+            //Kod här
+            
+
+            //20. Skriv ut till konsolen texten: Ange ett annat heltal:
+            //Kod här
+            
+
+            //21. Läs in ett annat heltal från konsolen tilldela värdet till variabel "input2"
+            //Kod här
+            
+
+            //22. Använd datatypen ints Parse funktion för att konvertera och tilldela
+            //värdet av "input1" till variabeln "tal1"
+            //Kod här
+
+            
+
+            //23. Använd datatypen ints Parse funktion för att konvertera och tilldela 
+            //värdet av "input2" till variabeln "tal2"
+            //Kod här
+
+            
+
+            //24. På en kod-rad skriv ut i konsolen mha string interpolation resultatet man 
+            //får av att multiplicera "tal1" och "tal2" med varandra.
+            //Exempel: Givet att "tal1" = 2 och "tal2" = 10 ska det skrivas ut i konsolen:
+            //Produkten av talen 2 och 10 är: 20
+            //Kod här
+
+            
+            
+        }
+    }
+}
