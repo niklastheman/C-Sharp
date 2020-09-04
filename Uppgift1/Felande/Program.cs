f134eaed3e79c52b8bf582882faa6a81745ad2a0
tackmats <tackmats@hotmail.com>
2020-09-04 11:54:13 +0200

    klar

--- Uppgift1/Felande/Program.cs
+++ Uppgift1/Felande/Program.cs
@@ -12,45 +12,49 @@ namespace Felande
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
-
+        {
             Console.WriteLine("Hej, välkommen till uppgift 1!");
             Console.WriteLine("Tryck på en knapp för att fortsätta...");
             Console.ReadKey();
 
-            namn = "Linda";
+            
 
-            Console.WriteLine;
-            Console.WriteLine($"Här ser du en sträng representerat av variablen 'namn': {namn}");
-            Console.WriteLine("Tryck på en knapp för att fortsätta...");
-            Console.ReadKey();
+                String namn = "Linda";
 
-            Console.WriteLine();
-            Console.WriteLine($"Boolean eller bool är en användbar datatyp när man vill avgör om något är sant eller falskt. Här ser vi variablen sants värde: {sant}");
-            Console.WriteLine("Tryck på en knapp för att fortsätta...");
-            Console.ReadKey();
+                Console.WriteLine();
+                Console.WriteLine($"Här ser du en sträng representerat av variablen 'namn': {namn}");
+                Console.WriteLine("Tryck på en knapp för att fortsätta...");
+                Console.ReadKey();
 
-            bool sant = false;
+                Console.WriteLine();
+                Console.WriteLine($"Boolean eller bool är en användbar datatyp när man vill avgör om något är sant eller falskt. Här ser vi variablen sants värde: {sant}");
+                Console.WriteLine("Tryck på en knapp för att fortsätta...");
+                Console.ReadKey();
 
-            DateTime datum = DateaTime.Now;
+                bool sant = false;
 
-            Console.WriteLine();
-            Console.ReadKey($"Här ser du ett datum representerat av variablen 'datum': {datum}")
-            ConsoleWriteLine("Tryck på en knapp för att fortsätta...");
-            Console.ReadKey();
+                DateTime datum = DateTime.Now;
 
-            decimal decimaltal = 0.5;
+                Console.WriteLine();
+                Console.WriteLine($"Här ser du ett datum representerat av variablen 'datum': {datum}");
+                Console.WriteLine("Tryck på en knapp för att fortsätta...");
+                Console.ReadKey();
 
-            Console.WriteLine();
-            Console.WriteLine($"Här ser du ett flyttal representerat av variablen 'decimaltal': {decimaltal});
-            Console.WriteLine("Tryck på en knapp för att fortsätta...");
+                decimal decimaltal = 0.5M;
 
-            string helttal = 30;
+                Console.WriteLine();
+                Console.WriteLine($"Här ser du ett flyttal representerat av variablen 'decimaltal': {decimaltal}");
+                Console.WriteLine("Tryck på en knapp för att fortsätta...");
 
-            Console.WriteLine();
-            Console.WriteLine($"Här ser du ett heltal representerat av variablen 'heltal': {helttal}");
-            Console.WriteLine("Tryck på en knapp för att fortsätta...");
-            Console.ReadKey();
+                int helttal = 30;
 
-            Console.ReadKey();
+                Console.WriteLine();
+                Console.WriteLine($"Här ser du ett heltal representerat av variablen 'heltal': {helttal}");
+                Console.WriteLine("Tryck på en knapp för att fortsätta...");
+                Console.ReadKey();
+
+                Console.ReadKey();
+            }
         }
-}
+    }
+
