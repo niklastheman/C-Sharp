a16002c25817d5719834a1d147945a2873274285
wallenas <peter.wallenas@gmail.com>
2020-09-09 10:03:01 +0200

    Inte klar! Test inför upstream!

--- Uppgift2/Villkor_och_loopar/Program.cs
+++ Uppgift2/Villkor_och_loopar/Program.cs
@@ -1,20 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+
 
 namespace Villkor_och_loopar
 {
 
     /// <summary>
-    
-       
+
+
     /// 
     /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            var x = Console.ReadLine();
-
+            //Lista som tar emot int-array
+            List<int[]> contestants = new List<int[]>();
             
+            bool isRunning = true;
+            int startNr, startTimme, startMinut, startSekund, malTimme, malMinut, malSekund;
+            while (isRunning)
+            {
+                Console.Write("Ange startnummer: ");
+                startNr = Convert.ToInt32(Console.ReadLine());
+                if (startNr < 1)
+                {
+                    isRunning = false;
+                    break;
+                }
+
+                int[] runner = new int[7]; // Om startnummer är större än 0 så skapas en ny löpare
+                runner[0] = startNr;
+
+                Console.Write("Ange timme för start: ");
+                startTimme = Convert.ToInt32(Console.ReadLine());
+                runner[1] = startTimme;
+                
+                Console.Write("Ange minut för start: ");
+                startMinut = Convert.ToInt32(Console.ReadLine());
+                runner[2] = startMinut;
+                
+                Console.Write("Ange sekund för start: ");
+                startSekund = Convert.ToInt32(Console.ReadLine());
+                runner[3] = startSekund;
+
+                Console.Write("Ange timme för mål: ");
+                malTimme = Convert.ToInt32(Console.ReadLine());
+                runner[4] = malTimme;
+
+                Console.Write("Ange minut för mål: ");
+                malMinut = Convert.ToInt32(Console.ReadLine());
+                runner[5] = malMinut;
+
+                Console.Write("Ange sekund för mål: ");
+                malSekund = Convert.ToInt32(Console.ReadLine());
+                runner[6] = malSekund;
+
+                contestants.Add(runner);
+
+            }
+            
+            //contestants.Sort();
+            //Console.WriteLine($"Startnr {contestants[0][0]} vann");
+            Console.WriteLine("Nu är spelet slut");
+            Console.ReadKey();
+        }
+
+        
+
+
+        public class Runner
+        {
+            public int StartNr { get; set; }
+            public int StartTimme { get; set; }
+            public int StartMinut { get; set; }
+            public int StartSekund { get; set; }
+            public int MalTimme { get; set; }
+            public int MalMinut { get; set; }
+            public int MalSekund { get; set; }
+
         }
     }
 }
+
--- Uppgift3/Klasser/Program.cs
+++ Uppgift3/Klasser/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Klasser
 {
@@ -10,14 +11,60 @@ namespace Klasser
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            List<Bil> Billista = new List<Bil>();
 
-            Console.WriteLine("Hello World!");
+            Bil BMW = new Bil
+            {
+                Model = "BMW Z3",
+                Vikt = 100,
+                Registrerades = DateTime.Now,
+                Elbil = false,
+                Registreringsnummer = "ABC 123"
+            };
+
+            Bil Tesla = new Bil
+            {
+                Model = "Tesla S-Series",
+                Vikt = 92,
+                Registrerades = DateTime.Now.ToString("2018-11-22"),
+                Elbil = true,
+                Registreringsnummer = "DEF 456"
+            };
+
+            Bil Volkswagen = new Bil
+            {
+                Model = "Volkswagen Passat",
+                Vikt = 110,
+                Registrerades = DateTime.Now,
+                Elbil = false,
+                Registreringsnummer = "GHI 789"
+            };
+
+            Billista.Add(BMW);
+            Billista.Add(Tesla);
+            Billista.Add(Volkswagen);
+
+            foreach (var Bil in Billista)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Model: {Bil.Model}");
+                Console.WriteLine($"Vikt: {Bil.Vikt}");
+                Console.WriteLine($"Registrerades: {Bil.Registrerades}");
+                Console.WriteLine($"Registreringsnummer: {Bil.Registreringsnummer}");
+                if (Bil.Elbil)
+                    Console.WriteLine("Den här är miljövänlig!");
+                
+            }
+            Console.ReadKey();
         }
     }
 
     public class Bil
     {
+        public string Model { get; set; }
+        public int Vikt { get; set; }
+        public string Registrerades { get; set; }  // Ändra eventuellt till string (public string Registrerades { get; set; }
+        public bool Elbil { get; set; }
         public string Registreringsnummer { get; set; }
-
     }
 }
